4baed734aa84f59f68a73c09b5f0b0ada6f57c93
Bas Milius <basmilius@hotmail.com>
2019-02-07 12:20:53 +0100

    Copyright.

--- script/rollup.config.prod.js
+++ script/rollup.config.prod.js
@@ -1,1 +1,11 @@
+/*
+ * Copyright (c) 2019 - IdeeMedia <info@ideemedia.nl>
+ * Copyright (c) 2019 - Bas Milius <bas@mili.us>
+ *
+ * This file is part of TypeWriter, a base framework for WordPress.
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
 export default {};
