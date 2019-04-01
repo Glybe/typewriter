016487ac0cdb8a1c743fc7c811e53262ae0fe4e8
Bas Milius <basmilius@hotmail.com>
2019-04-01 16:54:39 +0200

    Tested some stuff with themes.

--- public/app/themes/ideemedia/functions.php
+++ public/app/themes/ideemedia/functions.php
@@ -1,9 +1,15 @@
 <?php
 declare(strict_types=1);
 
+use TypeWriter\Facade\Hooks;
 use function TypeWriter\tw;
 
 tw()->getRouter()->get('/test', function (): string
 {
 	return 'Hi from router!';
+});
+
+Hooks::filter('style_loader_tag', function (string $tag, string $handle, string $href, string $media): string
+{
+	return sprintf('<link rel="stylesheet" href="%s" id="%s-id" media="%s" type="text/css" />', $href, $handle, $media);
 });
--- public/app/themes/ideemedia/index.php
+++ public/app/themes/ideemedia/index.php
@@ -1,1 +1,6 @@
-Hello world!
+<?php
+declare(strict_types=1);
+
+the_post();
+the_title('<h1>', '</h1>');
+the_content();
