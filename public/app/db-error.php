aaa392eb2952b72f5529f902bd022a730bac4421
Bas Milius <basmilius@hotmail.com>
2019-04-10 12:21:05 +0200

    Latte-ui update.

--- public/app/db-error.php
+++ public/app/db-error.php
@@ -6,7 +6,7 @@
 	<meta name="robots" content="noindex,nofollow"/>
 	<title>Maintenance</title>
 	<link rel="stylesheet" href="//cdn.mili.us/font/proxima-nova/proxima-nova.min.css"/>
-	<link rel="stylesheet" href="//unpkg.com/@bybas/latte-ui@1.5.0-beta.5/dist/latte.css"/>
+	<link rel="stylesheet" href="//unpkg.com/@bybas/latte-ui@1.5.0/dist/latte.css"/>
 	<style type="text/css">
 		html
 		{
