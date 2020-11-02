8d40918336ca8de437dcdae36cddcae954f6055d
Bas Milius <basmilius@hotmail.com>
2020-11-02 10:25:18 +0100

    chore: branding change.

--- public/app/maintenance.php
+++ public/app/maintenance.php
@@ -31,7 +31,7 @@ http_response_code(503);
 
         h1
         {
-            color: #4b8aff;
+            color: #0b63e9;
             font-size: 24px;
         }
 
@@ -49,7 +49,7 @@ http_response_code(503);
 
         a
         {
-            color: #057dcd;
+            color: #0b63e9;
             text-decoration-color: hsl(212deg, 26.3157894737%, 88.8235294118%);
         }
 
@@ -61,17 +61,7 @@ http_response_code(503);
 </head>
 <body>
 <main>
-    <svg height="54" viewBox="0 0 117.19 117.19">
-        <defs>
-            <linearGradient id="a" x1="101.82" y1="101.82" x2="15.37" y2="15.37" gradientUnits="userSpaceOnUse">
-                <stop offset="0" stop-color="#4b8aff"/>
-                <stop offset="1" stop-color="#377dff"/>
-            </linearGradient>
-        </defs>
-        <rect x="7.5" y="7.5" width="105" height="105" rx="31.65" transform="rotate(21 63.082 55.502)" fill="#4be4ff" fill-opacity=".85"/>
-        <rect x="6.1" y="6.1" width="105" height="105" rx="31.65" fill="url(#a)"/>
-        <path d="M39.48 34.6H63.1c8.78 0 13.68 5.47 13.68 12.24 0 6.05-3.89 10.08-8.35 11 5.11.8 9.29 5.91 9.29 11.81 0 7.42-5 13-14 13H39.48zm21.89 19.86c4.32 0 6.77-2.59 6.77-6.19S65.69 42 61.37 42H47.91v12.46zm.36 20.74c4.61 0 7.35-2.52 7.35-6.7 0-3.6-2.52-6.62-7.35-6.62H47.91V75.2z" fill="#fff"/>
-    </svg>
+    <img src="https://cdn.glybe.nl/public/brand/SVG/logo.svg" alt="Glybe-logo" height="54" width="54"/>
 
     <h1>Maintenance - <?= $_SERVER['HTTP_HOST'] ?></h1>
     <p>
@@ -81,7 +71,7 @@ http_response_code(503);
     <hr/>
     <p style="font-size: .8rem">
         This website is powered by <a href="https://github.com/basmilius/typewriter" rel="noopener" target="_blank">TypeWriter</a>,
-        which is created by <a href="https://bas.dev" rel="noopener" target="_blank">Bas Milius</a>.
+        which is created by <a href="https://glybe.nl" rel="noopener" target="_blank">Glybe.nl</a>.
     </p>
 </main>
 </body>
