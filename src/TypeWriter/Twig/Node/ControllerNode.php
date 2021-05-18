c06373071f83382d82d2f91960c88cad6dbfe4f2
Bas Milius <basmilius@hotmail.com>
2021-05-18 11:29:54 +0200

    fix(twig): remove new.

--- src/TypeWriter/Twig/Node/ControllerNode.php
+++ src/TypeWriter/Twig/Node/ControllerNode.php
@@ -43,7 +43,7 @@ final class ControllerNode extends Node
     {
         $compiler->addDebugInfo($this);
 
-        $compiler->write('$controller = new ' . Singleton::class . '::get(' . $this->getAttribute('controller') . ');' . PHP_EOL);
+        $compiler->write('$controller = ' . Singleton::class . '::get(' . $this->getAttribute('controller') . ');' . PHP_EOL);
         $compiler->write('$context = array_merge($context, $controller->getContext());');
     }
 
